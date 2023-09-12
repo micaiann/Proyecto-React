c066fd5bb727c7aa722f0bf32674c0b8fde2c014
MicaIann <micaela.iann99@gmail.com>
2023-09-12 11:51:21 -0300

    Modificación de la rama beneficioslectura

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import BeneficiosSection from '@/components/organisms/GrupoDeImagenes'
+import GrupoDeImagenes from '@/components/organisms/GrupoDeImagenes'
 
 const inter = Inter({ subsets: ['latin'] })
 
