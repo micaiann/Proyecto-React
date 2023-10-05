b9535fdbb03c9748b6b3954d621f0c5d9625bf2a
Mac <67873064+Macarena1212@users.noreply.github.com>
2023-10-05 00:56:11 -0300

    modal instalado, falta funcionalidad

--- src/components/atoms/clásicos/Product.js
+++ src/components/atoms/clásicos/Product.js
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
+import styles from "src/components/molecules/Modal/Modal.module.css";
 
 const Product = ({ imageSource, title, price, id, addToCart }) => {
+  const [modal, setModal] = useState(false);
 
-  
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  useEffect(() => {
+    if (modal) {
+      document.body.classList.add("active-modal");
+      document.body.style.overflow = "hidden"; // Saca el scrolleo
+    } else {
+      document.body.classList.remove("active-modal");
+      document.body.style.overflow = "auto"; // Repone el scrolleo
+    }
+  }, [modal]);
+
   return (
-    <div className="card">
-      <Image
-        className="img"
-        src={imageSource}
-        alt="img"
-        width={130}
-        height={200}
-      />
-      <h1>{title}</h1>
-      <h2>${price}</h2>
-      <button onClick={() => addToCart(id)}>Añadir</button>
-    </div>
+    <>
+      <div className="card" onClick={toggleModal}>
+        <Image
+          className="img"
+          src={imageSource}
+          alt="img"
+          width={130}
+          height={200}
+        />
+        <h1>{title}</h1>
+        <h2>${price}</h2>
+        <button onClick={() => addToCart(id)}>Añadir</button>
+      </div>
+
+      {modal && (
+        <div className={styles.modal}>
+          <div onClick={toggleModal} className={styles.overlay}></div>
+          <div className={styles["modal-content"]}>
+            <span className={styles["close-modal"]} onClick={toggleModal}>
+              &times;
+            </span>
+            <Image
+              className="img"
+              src={imageSource}
+              alt="img"
+              width={260}
+              height={400}
+            />
+            <h1>{title}</h1>
+            <h2>${price}</h2>
+          </div>
+        </div>
+      )}
+    </>
   );
-}
+};
 
 export default Product;
+
